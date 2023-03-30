2202c6ffac4dad8bed4d4ccf00297a39e2ef6545
Dzmitry Karneyenka <dzmitry_karneyenka@epam.com>
2023-03-30 14:21:23 +0300

    feat: add test data file

---
+++ src/data/cloudxinfo.data.ts
@@ -0,0 +1,21 @@
+export default {
+    /** Private Instance data */
+    PrivateInstance: {
+        InstanceId: "",
+        PrivateDns: "",
+        PrivateIp: "",
+        ImageId: "ami-037953870cfa44175",
+        PlatformDetails: "Linux/UNIX",
+    },
+
+    /** Public Instance data */
+    PublicInstance: {
+        InstanceId: "",
+        PrivateDns: "",
+        PrivateIp: "",
+        PublicDns: "",
+        PublicIp: "",
+        ImageId: "ami-037953870cfa44175",
+        PlatformDetails: "Linux/UNIX",
+    },
+};
